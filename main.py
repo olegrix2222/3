1d6eebeaeb1f51870e3e5a2046157b56bdfd7a85
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-03 16:17:02 +0000

    Add Zakhar and Dima

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class Cat:
 me = Person("Oleg", 11)
 my_pet = Cat("Murzik") 
 my_friend =Person("Makar", 11)
+my_friend2 =Person("Zakhar", 12)
+my_friend3 =Person("Dima", 15)
 
 print(me.is_sad)
 my_pet.play_w_human(me)
@@ -27,5 +29,14 @@ print("Sad - ", me.is_sad)
 print(my_friend.is_sad)
 my_pet.play_w_human(my_friend)
 print("Sad - ", my_friend.is_sad)
+
+print(my_friend2.is_sad)
+my_pet.play_w_human(my_friend2)
+print("Sad - ", my_friend2.is_sad)
+
+print(my_friend3.is_sad)
+my_pet.play_w_human(my_friend3)
+print("Sad - ", my_friend3.is_sad)
+
 
 
